f2c7cfadece55f4f0c60a7e0e1ce034e632be28f
maria <maria77julia@gmail.com>
2021-06-17 13:08:20 +0200

    ammend BookWriteView

--- Books/api_routes.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import routers
-from api import views as api_views
-
-
-router = routers.DefaultRouter()
-router.register(r'books', api_views.BookViewSet)
--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import generics
+from rest_framework import mixins
 from rest_framework import status
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -10,7 +12,7 @@ from api.utils import download_books
 from api.filters import BookFilterSet
 
 
-class BookViewSet(viewsets.ModelViewSet):
+class BookViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     filterset_class = BookFilterSet
@@ -25,11 +27,13 @@ class BookViewSet(viewsets.ModelViewSet):
         return queryset
 
 
-    @action(detail=False, methods=['post'])
-    def db(self, request):
-        q = request.data.get('q')
+class BookWriteView(generics.CreateAPIView):
+    serializer_class = BookWriteSerializer
+
+    def post(self, request, *args, **kwargs):
+        q = self.request.data.get('q')
         books = download_books(q)
-        serializer = BookWriteSerializer(data=books, many=True)
+        serializer = self.serializer_class(data=books, many=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
